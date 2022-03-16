d4cb74fd60e92157c9e4b9755dc2dad52ea9083f
LILKOTYO <lil-kotyo@sjtu.edu.cn>
2022-03-16 14:37:06 +0800

    (2022/3/16) pspheregenerator update

--- 04 Particle System/04 Particle System/include/pgenerator.h
+++ 04 Particle System/04 Particle System/include/pgenerator.h
@@ -61,8 +61,8 @@ std::vector<shared_ptr<particle>> ppointgenerator::generateOmni(int frequency, i
 			color = ColorList[i];
 			time = lifetime + static_cast<int>(0.25 * lifetime * GaussianList[i]);
 		}
-
-		glm::vec3 local_v = uvw.world(glm::vec3(cos(theta), h, -sin(theta)));
+		float r = sqrt(1 - pow(h, 2));
+		glm::vec3 local_v = uvw.world(glm::vec3(r * cos(theta), h, -r * sin(theta)));
 		shared_ptr<particle> p = make_shared<particle>(x0, local_v, color, time);
 		result.push_back(p);
 
@@ -136,7 +136,7 @@ std::vector<shared_ptr<particle>> ppointgenerator::generateDir(int frequency, in
 class pdiscgenerator : public pgenerator {
 public:
 	pdiscgenerator() {}
-	pdiscgenerator(glm::vec3 x, float radius, glm::vec3 n) : c(x), r(radius),normal(n) {
+	pdiscgenerator(glm::vec3 x, float radius, glm::vec3 n) : c(x), R(radius),normal(n) {
 		uvw.build(normal);
 	}
 
@@ -144,7 +144,7 @@ public:
 public:
 	// centor, radius of the disc and the normal.
 	glm::vec3 c; 
-	float r;
+	float R;
 	glm::vec3 normal;
 };
 
@@ -184,9 +184,80 @@ std::vector<shared_ptr<particle>> pdiscgenerator::generate(int frequency, int du
 			time = lifetime + static_cast<int>(0.25 * lifetime * GaussianList[i]);
 		}
 		phi = sqrt(h) * sigma;
-		offset = sqrt(f) * r;
+		offset = sqrt(f) * R;
 		glm::vec3 local_v = uvw.world(glm::vec3(cos(theta) * sin(phi), sin(theta) * sin(phi), cos(phi)));
 		glm::vec3 local_x0 = c + uvw.world(glm::vec3(offset * cos(theta_disc), offset * sin(theta_disc), 0.0));
+		shared_ptr<particle> p = make_shared<particle>(local_x0, local_v, color, time);
+
+		result.push_back(p);
+
+		std::chrono::steady_clock::time_point end_t = std::chrono::steady_clock::now();
+
+		time_cost = std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t).count();
+		if (time_cost < step_ms)
+		{
+			Sleep(step_ms - time_cost);
+		}
+		else
+		{
+			std::cout << "generate slowly ..." << std::endl;
+		}
+	}
+	return result;
+}
+
+class pspheregenerator : public pgenerator {
+public:
+	pspheregenerator() {}
+	pspheregenerator(glm::vec3 x, float radius) : c(x), R(radius) {}
+
+	std::vector<shared_ptr<particle>> generate(int frequency, int dur_s, int lifetime, float sigma);
+public:
+	glm::vec3 c;
+	float R;
+};
+
+std::vector<shared_ptr<particle>> pspheregenerator::generate(int frequency, int dur_s, int lifetime, float sigma) {
+	int count = frequency * dur_s;
+	int step_ms = static_cast<int>(dur_s * 1000 / frequency);
+	std::vector<shared_ptr<particle>> result;
+
+	for (int i = 0; i < count; i++)
+	{
+		std::chrono::steady_clock::time_point start_t = std::chrono::steady_clock::now();
+		float theta_sphere;
+		float y;
+		float offset;
+		float theta;
+		float h;
+		float phi;
+		glm::vec3 color;
+		int time;
+		int time_cost;
+		if (i > randomcount - 1)
+		{
+			theta = UniformList[i - randomcount] * pi;
+			h = 0.5 * (UniformList[2 * randomcount - 1 - i] + 1.0);
+			theta_sphere = UniformList[2 * randomcount - 1 - i] * pi;
+			y = UniformList[i - randomcount];
+			color = ColorList[i - randomcount];
+			time = lifetime + static_cast<int>(0.25 * lifetime * GaussianList[i - randomcount]);
+		}
+		else
+		{
+			theta = UniformList[i] * pi;
+			h = 0.5 * (1.0 + UniformList[randomcount - 1 - i]);
+			theta_sphere = UniformList[randomcount - 1 - i] * pi;
+			y = UniformList[i];
+			color = ColorList[i];
+			time = lifetime + static_cast<int>(0.25 * lifetime * GaussianList[i]);
+		}
+		phi = sqrt(h) * sigma;
+		float r = sqrt(1 - pow(y, 2));
+		glm::vec3 normal = glm::vec3(r * cos(theta_sphere), y, -r * sin(theta_sphere));
+		uvw.build(normal);
+		glm::vec3 local_v = uvw.world(glm::vec3(cos(theta) * sin(phi), sin(theta) * sin(phi), cos(phi)));
+		glm::vec3 local_x0 = c + R * normal;
 		shared_ptr<particle> p = make_shared<particle>(local_x0, local_v, color, time);
 
 		result.push_back(p);
